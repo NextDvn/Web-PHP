8da8823cf9483ddb75c785aa57534077d4991779
Devon <53498529+NextDvn@users.noreply.github.com>
2020-03-03 20:12:50 +0700

    Add files via upload

---
+++ Modul 2 Web PHP/Lat 7.php
@@ -0,0 +1,7 @@
+<?php
+
+$arrbuah =  array ("Mangga","Apel","Pisang","Jeruk");
+echo $arrbuah[0];
+echo $arrbuah[3];
+
+?>
---
+++ Modul 2 Web PHP/Lat 8.php
@@ -0,0 +1,7 @@
+<?php
+
+$arrnilai =  array ("Ani" => 80,"Otim" => 90,"Ana" => 75,"Budi" => 85);
+echo $arrnilai["Ani"];
+echo $arrnilai["Otim"];
+
+?>
